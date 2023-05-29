6e7760dd33536d3d0fd062ce7e1f714d3ba9b6f0
mjkam <mjkam@naver.com>
2023-05-29 15:44:45 +0900

    update

---
+++ src/main/java/com/example/demo/MyController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class MyController {
+    @GetMapping("/deploy")
+    public String test() {
+        return "deploy version 1: " + UUID.randomUUID();
+    }
+}
